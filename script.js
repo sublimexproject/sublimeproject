61db79385c0240938b0a307cb547de7a43bdeb7b
Sublime Project <sublimexproject@gmail.com>
2026-03-16 14:52:34 -0500

    fix duplicated page

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const verses = [ 
     "John 3:16",
     "Proverbs 3:5",
@@ -8,15 +7,4 @@ const verses = [
 
 const randomIndex = Math.floor(Math.random() * verses.length);
 
-=======
-const verses = [ 
-    "John 3:16",
-    "Proverbs 3:5",
-    "Romans 8:28",
-    "Phillipians 4:13"
-];
-
-const randomIndex = Math.floor(Math.random() * verses.length);
-
->>>>>>> e8b31e9e10f953f3e388e40da34e79afdb494083
 document.getElementById("verse").textContent = verses[randomIndex];
